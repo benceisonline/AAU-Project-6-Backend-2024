004c691d48658ef81d27b052bc77169084ccaac7
Louise Steffens <62352035+loui981c@users.noreply.github.com>
2024-03-15 12:49:09 +0100

    init commit (#6)

--- Collaborative_Filtering/EBNERD_utilities.py
+++ Collaborative_Filtering/EBNERD_utilities.py
@@ -40,6 +40,7 @@ def initialize_model(ind2user, ind2item):
 # Function to make predictions
 def make_predictions(request, model, user2ind, ind2article, news_data):
     user_id = int(request.user_id)  # Convert user_id to int
+    no_recommendations = request.no_recommendations
 
     # Get user index
     user_idx = user2ind.get(user_id)
@@ -51,11 +52,17 @@ def make_predictions(request, model, user2ind, ind2article, news_data):
     # If user_id is not found, raise an HTTPException
     if user_idx is None:
         raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+    
+    if no_recommendations is None:
+        raise HTTPException(status_code=404, detail=f"Please provide a valid number of recommendations.")
+    
+    if no_recommendations < 1:
+        raise HTTPException(status_code=404, detail=f"Please provide a number of recommendations over 0.")
 
     item_ids = list(ind2article.keys())
 
     predictions = model.forward(torch.tensor([user_idx] * len(item_ids)), torch.tensor(item_ids))
-    top_indices = torch.topk(predictions.flatten(), 10).indices
+    top_indices = torch.topk(predictions.flatten(), no_recommendations).indices
     top_item_ids = [ind2article[ix.item()] for ix in top_indices]
     
     # Explicitly type all column names
