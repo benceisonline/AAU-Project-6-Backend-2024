ef2765563e6d2a35c504b76ed516fdd2409ba2ee
Bence Szabo <benceszabo657@gmail.com>
2024-03-19 15:04:04 +0100

    image url generation (#8)

--- Collaborative_Filtering/EBNERD_utilities.py
+++ Collaborative_Filtering/EBNERD_utilities.py
@@ -1,8 +1,11 @@
-from fastapi import HTTPException
-import torch
 import pandas as pd
+import torch
+import hashlib
+from fastapi import HTTPException
 from newsmf_model import NewsMF
+import numpy as np  # Import NumPy
 
+# Function to load data
 def load_data():
     # Load news data
     news_data = pd.read_parquet("./ebnerd_small/articles.parquet")
@@ -31,11 +34,38 @@ def load_data():
 
     return news_data, ind2article, article2ind, ind2user, user2ind
 
+# Function to initialize model
 def initialize_model(ind2user, ind2item):
     model = NewsMF(num_users=len(ind2user) + 1, num_items=len(ind2item) + 1)
     model.load_state_dict(torch.load("Saved_Model/EBNERD_collaborative_filtering_model.pth"))
     model.eval()
     return model
+
+# Function to generate image URLs
+def generate_image_url(image_id, article_id, published_timestamp, modified_timestamp):
+    hash_string = f"caravaggio-{article_id}-{published_timestamp}-{modified_timestamp}"
+    md5_hash = hashlib.md5(hash_string.encode()).hexdigest()
+    return f"https://img-cdn-p.ekstrabladet.dk/image/ekstrabladet/{image_id}/relationBig_910/?at={md5_hash}"
+
+def generate_image_urls(recommended_items):
+    for item in recommended_items:
+        article_id = item['article_id']
+        image_ids = item.get('image_ids')
+        if image_ids is not None and len(image_ids) > 0:  # Check if image_ids is not None and is not empty
+            image_id = image_ids[0]  # Assuming only the first image is used
+            published_timestamp = item['published_time'].value / 1000000  # convert to seconds
+            modified_timestamp = item['last_modified_time'].value / 1000000  # convert to seconds
+            item['image_url'] = generate_image_url(image_id, article_id, published_timestamp, modified_timestamp)
+        else:
+            # If no image IDs are available, use default image URL
+            item['image_url'] = "https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/66/53/28/66532893-b30e-3023-20be-448c4d19428f/AppIcon-0-0-1x_U007ephone-0-85-220.png/1200x630wa.png"
+
+        # Convert NumPy array to list
+        if isinstance(item['image_ids'], np.ndarray):
+            item['image_ids'] = item['image_ids'].tolist()
+
+    return recommended_items
+
 
 # Function to make predictions
 def make_predictions(request, model, user2ind, ind2article, news_data):
@@ -65,17 +95,14 @@ def make_predictions(request, model, user2ind, ind2article, news_data):
     top_indices = torch.topk(predictions.flatten(), no_recommendations).indices
     top_item_ids = [ind2article[ix.item()] for ix in top_indices]
     
-    # Explicitly type all column names
-    all_column_names = ["article_id", "title", "subtitle", "last_modified_time", "premium", "body",
-                    "published_time", "image_ids", "article_type", "url", "entity_groups", "topics",
-                    "category", "subcategory", "category_str", "total_inviews", "total_pageviews",
-                    "total_read_time", "sentiment_score", "sentiment_label"]
-    
-    # image_ids, enitity_groups, topics, subcategory, total_inviews, total_pageviews, total_read_time are freaky for some reason
+    # Extract relevant columns for recommendation
     column_names = ["article_id", "title", "subtitle", "last_modified_time", "premium", "body", "published_time", 
-                    "article_type", "url", "category", "category_str", "sentiment_score", "sentiment_label"]
+                    "article_type", "url", "category", "category_str", "sentiment_score", "sentiment_label", "image_ids"]
 
-    # Extract all columns from news_data
+    # Extract recommended items
     recommended_items = news_data[news_data["article_id"].isin(top_item_ids)][column_names].to_dict(orient='records')
+    
+    # Generate image links for recommended items
+    generate_image_urls(recommended_items)
 
     return {"user_id": user_id, "recommended_items": recommended_items}
