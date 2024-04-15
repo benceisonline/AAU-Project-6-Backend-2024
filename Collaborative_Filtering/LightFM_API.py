a861687b3bf239d2f7a450631f6bc7a215be66c8
Louise Steffens <foldoysteffenslouise@gmail.com>
2024-04-15 18:37:45 +0200

    fetch preds / all correctly

--- Collaborative_Filtering/LightFM_API.py
+++ Collaborative_Filtering/LightFM_API.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 app = FastAPI()
 
-url = "192.168.1.31"
+url = "172.20.10.4"
 
 news_data = pd.read_parquet("./ebnerd_small/articles.parquet")
 news_tools = NewsTools(news_data)
@@ -25,27 +25,31 @@ recommender_system.load_model()
 # Pydantic model for request payload
 class PredictionRequest(BaseModel):
     user_id: str
+    start_index: int
     no_recommendations: int
 
 # Pydantic model for response payload
 class PredictionResponse(BaseModel):
-    news: list[NewsItem]
+    news: list[object]
 
 # Endpoint for making predictions
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
-    result = recommender_system.make_predictions_for_user(request, news_data)
-    news_list = result["news"]
-    news_tools.generate_image_urls(news_list)
+    result = recommender_system.make_predictions_for_user(request)
+    news_tools.generate_image_urls(result['news'])
     return result
 
 # Pydantic model for response payload
+class AllNewsRequest(BaseModel):
+    start_index: int
+    no_recommendations: int
+
 class AllNewsResponse(BaseModel):
-    news: list[NewsItem]
+    news: list[object]
 
 @app.get("/all", response_model=AllNewsResponse)
-def all_news():
-    return news_tools.get_newest_news(news_data)
+def all_news(request: AllNewsRequest):
+    return news_tools.get_newest_news(request)
 
 # Default route to handle requests to the root endpoint
 @app.get("/")
